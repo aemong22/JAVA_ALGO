65847ec9633a7fcb17c1bdb32091c0b620e75fad
pinkpig_21 <pinkpig_21@naver.com>
2022-08-05 17:48:47 +0900

    8월 05일

---
+++ CLASS_02/src/study/B_1874.java
@@ -0,0 +1,25 @@
+package study;
+
+// ctrl + shift + o => 자동으로 import
+// 코드정렬 : ctrl + a 누른후 ctrl shift i
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1874 {
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		
+		int t = Integer.parseInt(st.nextToken());
+		for(int i = 0 ; i < t ; i ++) {
+			
+		}
+	}
+
+}
