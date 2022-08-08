0972108013a5b802384810223ef54f2312fc6b69
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-08-08 17:57:57 +0900

    백준-자바

--- CLASS_02/src/study/B_2775.java
+++ CLASS_02/src/study/B_2775.java
@@ -7,10 +7,38 @@ import java.util.StringTokenizer;
 public class B_2775 {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		StringBuilder sb = new StringBuilder();
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int t = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < t; i++) {
+			st = new StringTokenizer(in.readLine());
+			int floor = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(in.readLine());
+			int room = Integer.parseInt(st.nextToken());
+			
+			int[] apt = new int[room];
+			
+			for(int j = 0; j < room ; j++) {
+				apt[j] = j+1;
+			}
+			
+			for(int j = 0; j < floor; j++) {
+				for(int z = 1;  z < room ; z++ ) {
+					apt[z] += apt[z-1];
+				}
+			}
+			
+			sb.append(apt[room-1]+"\n");
+		}
+		
+		System.out.println(sb);
+	
 	}
+	
+	
 
 }
