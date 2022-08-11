1cd9933818b38189dc7b06e0ef21c0ea08759468
[4기-1반]김애림 <pinkpig_21@naver.com>
2022-08-11 22:34:26 +0900

    .

---
+++ CLASS_02/src/study/B_4949.java
@@ -0,0 +1,14 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_4949 {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
