00bbb5dc582e13a59122fb704e52775a5a1c22c2
aerim <pinkpig_21@naver.com>
2022-09-16 16:50:10 +0900

    0916

--- CLASS_03/src/study/B_5430.java
+++ CLASS_03/src/study/B_5430.java
@@ -1,10 +1,70 @@
 package study;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class B_5430 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(in.readLine());
+		for(int tc = 0; tc < t; tc++) {
+			// 함수 입력
+			String[] p = in.readLine().trim().split("");
+			ArrayList<String> answer = new ArrayList<>();
+			// 배열 크기 입력
+			int n = Integer.parseInt(in.readLine());
+			
+			String str = in.readLine();
+			str = str.substring(1,str.length()-1);
+			Deque<String> dq = new ArrayDeque<>(Arrays.asList(str.split(",")));
+			boolean end = false;
+			boolean err = false;
+			for(int i = 0; i < p.length; i++) {
+				switch (p[i]) {
+				case "R":
+					end = !end;
+					break;
 
+				case "D":
+					if(n == 0 || dq.isEmpty()) {
+						err = true;
+						break;
+					}
+					else {
+						String temp = end ? dq.pollLast()  : dq.pollFirst();
+					}
+					break;
+				}
+				if(err) break;
+			}
+			
+			if(err) {
+				sb.append("error\n");
+			}
+			else {
+				sb.append("[");
+				int temp = dq.size();
+				for(int i = 0; i < temp; i++) {
+					sb.append(end ? dq.pollLast() : dq.pollFirst());
+					if(i != temp-1) {
+						sb.append(",");
+					}
+					
+				}
+				sb.append("]\n");
+				
+			}
+			
+		}
+		
+		System.out.println(sb);
+		
 	}
 
 }
---
+++ CLASS_03/src/study/B_5525.java
@@ -0,0 +1,16 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B_5525 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		int l = Integer.parseInt(in.readLine());
+		String str = in.readLine().trim();
+
+	}
+
+}
